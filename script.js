3b385dbff92331a2b250aac6631a4dd6b3f3262a
dalifyyy <d4lified@gmail.com>
2026-02-10 22:03:13 +0800

    ..

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function file_to_base64() {
-    
+
+
+const  file_to_base64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+});
+
+function base64_to_audio() {
+
+}
+
+function base64_to_file() {
+
+
 }
